fdacefb96ceabb3867d4729614749f1e89fee841
fsarachu <fsarachu@gmail.com>
2017-03-11 06:16:16 -0300

    Basic class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
-import sayHi from './SayHi';
+import Drone from './Drone';
 
-sayHi();
+let drone = new Drone('P154', 'Parrot AR');
 
-
+console.log(`drone: ${drone}`);
+console.log(`typeof Drone: ${typeof Drone}`);
+console.log(`typeof drone: ${typeof drone}`);
+console.log(`drone instanceof Drone: ${drone instanceof Drone}`);
+console.log(`drone instanceof Object: ${drone instanceof Object}`);
