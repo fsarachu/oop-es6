8d8c104de18ffddba78d5d87de4e969236745109
fsarachu <fsarachu@gmail.com>
2017-03-11 12:36:12 -0300

    Add "airHours" attribute to Drone

--- app/scripts/models/Drone.js
+++ app/scripts/models/Drone.js
@@ -2,11 +2,16 @@ import Vehicle from './Vehicle';
 
 export default class Drone extends Vehicle {
 
-  constructor(id, licenseNumber, make, model) {
-    super(id, licenseNumber, make, model);
+  constructor(id, licenseNumber, make, model, position, airHours) {
+    super(id, licenseNumber, make, model, position);
+    this._airHours = airHours;
   }
 
-  start() {
-    console.log('Starting Drone');
+  get airHours() {
+    return this._airHours;
+  }
+
+  set airHours(value) {
+    this._airHours = value;
   }
 }
