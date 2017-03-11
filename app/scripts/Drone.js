5b20e6fdd2b7a6e8b1b65ff51f735921a0c1b40c
fsarachu <fsarachu@gmail.com>
2017-03-11 08:49:56 -0300

    Class Inheritance

--- app/scripts/Drone.js
+++ app/scripts/Drone.js
@@ -1,53 +1,5 @@
-export default class Drone {
-  constructor(id, name, maxSpeed) {
-    this._id = id;
-    this._name = name;
-    this._maxSpeed = maxSpeed;
-  }
+import Vehicle from './Vehicle';
 
-  get id() {
-    console.log('Accesing id property');
-    return this._id;
-  }
+export default class Drone extends Vehicle {
 
-  set id(value) {
-    console.log(`Setting id property from ${this._id} to ${value}`);
-    this._id = value;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
-  get maxSpeed() {
-    return this._maxSpeed;
-  }
-
-  set maxSpeed(value) {
-    this._maxSpeed = value;
-  }
-
-  static theFastestOf(drone1, drone2) {
-    let fastest = null;
-
-    if (drone1.maxSpeed > drone2.maxSpeed) {
-      fastest = drone1;
-    }
-
-    if (drone1.maxSpeed < drone2.maxSpeed) {
-      fastest = drone2;
-    }
-
-    return fastest;
-  }
-
-  fly() {
-    console.log(`Drone ${this.id} is flying.`);
-  }
 }
-
-Drone.vehicleType = 'Rotorcraft';
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,14 @@
+import Vehicle from './Vehicle';
 import Drone from './Drone';
+import Car from './Car';
 
-let drone1 = new Drone('PA154', 'Parrot AR', 50);
-let drone2 = new Drone('PH412', 'Phantom DJI', 32);
+let drone = new Drone();
+let car = new Car();
 
-console.log('drone1.fly()');
-drone1.fly();
-console.log('drone2.fly()');
-drone2.fly();
+console.log(`drone instanceof Drone: ${drone instanceof Drone}`);
+console.log(`drone instanceof Vehicle: ${drone instanceof Vehicle}`);
+console.log(`drone instanceof Object: ${drone instanceof Object}`);
 
-console.log('Drone.theFastestOf(drone1, drone2):');
-console.dir(Drone.theFastestOf(drone1, drone2));
-
-console.log(`drone1.id: ${drone1.id}`);
-drone1.id = 'PA100';
-console.log(`drone1.id: ${drone1.id}`);
-
+console.log(`car instanceof Car: ${car instanceof Car}`);
+console.log(`car instanceof Vehicle: ${car instanceof Vehicle}`);
+console.log(`car instanceof Object: ${car instanceof Object}`);
