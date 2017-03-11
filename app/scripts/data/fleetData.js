e80ff3c95e23578040a338b6a64962478939e531
fsarachu <fsarachu@gmail.com>
2017-03-11 13:09:30 -0300

    Add fleetData

---
+++ app/scripts/data/fleetData.js
@@ -0,0 +1,114 @@
+export default fleet = [
+  {
+    type: 'car',
+    license: 'C4928',
+    make: 'BMW',
+    model: '120i',
+    kilometers: 122396,
+    position: {lat: -34.899616, lng: -54.990685}
+  },
+  {
+    type: 'car',
+    license: 'C3765',
+    make: 'BMW',
+    model: '118i',
+    kilometers: 8364,
+    position: {lat: -34.911362, lng: -54.977070}
+  },
+  {
+    type: 'car',
+    license: 'C0283',
+    make: 'Honda',
+    model: 'Civic',
+    kilometers: 28375,
+    position: {lat: -34.905933, lng: -54.947909}
+  },
+  {
+    type: 'car',
+    license: 'C1030',
+    make: 'Ford',
+    model: 'Focus',
+    kilometers: 549302,
+    position: {lat: -34.909417, lng: -54.999686}
+  },
+  {
+    type: 'car',
+    license: 'C4937',
+    make: 'Audi',
+    model: 'Q3',
+    kilometers: 30589,
+    position: {lat: -34.936212, lng: -54.949325}
+  },
+  {
+    type: 'car',
+    license: 'C8967',
+    make: 'Suzuki',
+    model: 'Swift',
+    kilometers: 81405,
+    position: {lat: -34.959332, lng: -54.942223}
+  },
+  {
+    type: 'drone',
+    license: 'D1023',
+    make: 'DJI',
+    model: 'Mavic',
+    airHours: 195,
+    position: {lat: -34.933925, lng: -54.907923}
+  },
+  {
+    type: 'drone',
+    license: 'D2948',
+    make: 'DJI',
+    model: 'Phantom 4',
+    airHours: 83,
+    position: {lat: -34.944505, lng: -54.954025}
+  },
+  {
+    type: 'drone',
+    license: 'D2090',
+    make: 'DJI',
+    model: 'Inspire 2',
+    airHours: 320,
+    position: {lat: -34.904401, lng: -54.974002}
+  },
+  {
+    type: 'drone',
+    license: 'D4293',
+    make: 'DJI',
+    model: 'Mavic',
+    airHours: 243,
+    position: {lat: -34.915496, lng: -54.944659}
+  },
+  {
+    type: 'drone',
+    license: 'D3676',
+    make: 'DJI',
+    model: 'Inspire 2',
+    airHours: 381,
+    position: {lat: -34.912786, lng: -54.978691}
+  },
+  {
+    type: 'drone',
+    license: 'D2309',
+    make: 'Yuneec',
+    model: 'Breeze',
+    airHours: 411,
+    position: {lat: -34.920158, lng: -54.879063}
+  },
+  {
+    type: 'drone',
+    license: 'D3302',
+    make: 'Yuneec',
+    model: 'Typhoon H',
+    airHours: 293,
+    position: {lat: -34.969214, lng: -54.954036}
+  },
+  {
+    type: 'drone',
+    license: 'D2112',
+    make: 'Parrot',
+    model: 'AR Drone 2.0',
+    airHours: 502,
+    position: {lat: -34.903706, lng: -54.929510}
+  },
+]
