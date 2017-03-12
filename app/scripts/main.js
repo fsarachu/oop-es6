761ee523a6a827f1a338796c3db5333078643b9c
fsarachu <fsarachu@gmail.com>
2017-03-12 17:06:18 -0300

    Add Image class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 import fleet from './data/fleetData';
 import FleetDataService from './services/FleetDataService';
 import Button from "./ui/Button";
+import Image from "./ui/Image";
 
 let dataService = new FleetDataService();
 dataService.loadData(fleet);
@@ -11,5 +12,10 @@ for (let e of dataService.errors) {
   console.error(e.message);
 }
 
+let $body = $('body');
+
 let button = new Button('Click Me!');
-button.appendToElement($('body'));
+button.appendToElement($body);
+
+let image = new Image('images/drone.jpg');
+image.appendToElement($body);
---
+++ app/scripts/ui/Image.js
@@ -0,0 +1,21 @@
+import BaseElement from "./BaseElement";
+
+export default class Image extends BaseElement {
+
+  constructor(url) {
+    super();
+    this._url = url;
+  }
+
+  get url() {
+    return this._url;
+  }
+
+  set url(value) {
+    this._url = value;
+  }
+
+  getElementString() {
+    return `<img src="${this.url}" style="width: 100%;">`;
+  }
+}
