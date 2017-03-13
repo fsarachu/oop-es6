2cf5e4eb98fcb65d1667e9af4fa39c36ba4d3164
fsarachu <fsarachu@gmail.com>
2017-03-13 16:10:50 -0300

    Add routing stuff to AppBase class

--- app/scripts/framework/AppBase.js
+++ app/scripts/framework/AppBase.js
@@ -4,6 +4,8 @@ export default class AppBase {
   constructor(title) {
     this._title = title;
     this._titleBar = new TitleBar('FleetTracker');
+    this._routeMap = [];
+    this._defaultRoute = null;
   }
 
   get title() {
@@ -22,7 +24,33 @@ export default class AppBase {
     this._titleBar = value;
   }
 
+  get routeMap() {
+    return this._routeMap;
+  }
+
+  set routeMap(value) {
+    this._routeMap = value;
+  }
+
+  get defaultRoute() {
+    return this._defaultRoute;
+  }
+
+  set defaultRoute(value) {
+    this._defaultRoute = value;
+  }
+
   render($parentElement) {
     this.titleBar.appendToElement($parentElement);
+  }
+
+  addRoute(title, url, pageObject, defaultRoute = false, icon = null) {
+    this.titleBar.addLink(title, url, icon);
+
+    this.routeMap[url] = pageObject;
+
+    if (defaultRoute) {
+      this.defaultRoute = url;
+    }
   }
 }
