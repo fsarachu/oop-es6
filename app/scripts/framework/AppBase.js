10b165c82371ab04591305be03d1fbdca86d0de8
fsarachu <fsarachu@gmail.com>
2017-03-13 15:04:28 -0300

    Define AppBase class

---
+++ app/scripts/framework/AppBase.js
@@ -0,0 +1,24 @@
+import TitleBar from '../ui/TitleBar';
+
+export default class AppBase {
+  constructor(title) {
+    this._title = title;
+    this._titleBar = new TitleBar('FleetTracker');
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
+  }
+
+  get titleBar() {
+    return this._titleBar;
+  }
+
+  set titleBar(value) {
+    this._titleBar = value;
+  }
+}
