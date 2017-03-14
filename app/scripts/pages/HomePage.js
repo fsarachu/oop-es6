047d8917b78b9ab41aebd822b426c3c7c412ae58
fsarachu <fsarachu@gmail.com>
2017-03-14 11:42:35 -0300

    Update HomePage to correctly extend BasePage

--- app/scripts/pages/HomePage.js
+++ app/scripts/pages/HomePage.js
@@ -1,14 +1,16 @@
-import BasePage from './BasePage';
+import BasePage from '../framework/BasePage';
 import Image from '../ui/Image';
 
 export default class HomePage extends BasePage {
 
-  constructor() {
-    super();
+  constructor(title) {
+    super(title);
   }
 
-  render() {
+  createElement() {
+    super.createElement();
+
     let image = new Image('images/drones.jpg');
-    image.appendToElement(this.container);
+    image.appendToElement(this.element);
   }
 }
