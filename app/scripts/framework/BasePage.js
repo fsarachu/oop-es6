fde9e502f9cd4d95b4ec2e5ff55c4ed0b0555cab
fsarachu <fsarachu@gmail.com>
2017-03-14 11:31:45 -0300

    Add title attribute to BasePage

--- app/scripts/framework/BasePage.js
+++ app/scripts/framework/BasePage.js
@@ -2,8 +2,17 @@ import BaseElement from './BaseElement';
 
 export default class BasePage extends BaseElement {
 
-  constructor() {
+  constructor(title) {
     super();
+    this._title = title;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
   }
 
   getElementString() {
