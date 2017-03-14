7494b9d99794a5f9e1ce3c55f0b53de040f6f40d
fsarachu <fsarachu@gmail.com>
2017-03-14 11:48:52 -0300

    Remove thrower override, so i can call BaseElement.createElement()

--- app/scripts/framework/BasePage.js
+++ app/scripts/framework/BasePage.js
@@ -20,9 +20,4 @@ export default class BasePage extends BaseElement {
   getElementString() {
     return '<div class="page-content"></div>';
   }
-
-  createElement() {
-    // This is where all the page is built and appended to this.element (page container)
-    throw new Error('You must override createElement() in BasePage');
-  }
 }
